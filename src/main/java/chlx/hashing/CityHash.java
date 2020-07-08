abc9ea16cdedb1aaa9d2afb8550b699c98784dbd
tuannm <tuannm@coccoc.com>
2020-07-08 16:35:51 +0700

    Fix ch

--- src/main/java/chlx/hashing/CityHash.java
+++ src/main/java/chlx/hashing/CityHash.java
@@ -1,7 +1,5 @@
 package chlx.hashing;
 /*
- * Copyright (C) 2012 coccoc.com
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
